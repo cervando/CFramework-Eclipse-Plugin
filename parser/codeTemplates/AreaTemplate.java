36d1e3db85c3691734858b5d63afce0a55e6b5eb
cervando <armando.cervantes.h@gmail.com>
2018-10-25 10:45:30 -0500

    Adeed Routes to template

--- parser/codeTemplates/AreaTemplate.java
+++ parser/codeTemplates/AreaTemplate.java
@@ -6,13 +6,25 @@ public class AreaTemplate {
 "\n" +
 "\nimport config.AreaNames;" +
 "\nimport kmiddle2.nodes.areas.Area;" +
-"\nimport kmiddle2.nodes.activities.ActConf;" +
+"\nimport kmiddle2.nodes.activities.ActConf;" + 
+"\nimport kmiddle2.communications.spikes.SpikeMerger;" + 
+"\nimport kmiddle2.communications.spikes.SpikeRouter;" +
 "\n" +
 "\npublic class %s extends Area{" +
 "\n	" +
 "\n	public %s(){" +
 "\n		this.ID = AreaNames.%s;" + 
 "\n		this.namer = AreaNames.class;" +
+"\n		" + 
+"\n		AddRoute(new SpikeRouter(" +
+"\n				new int[]{ AreaNames.X, AreaNames.Y}," + 
+"\n				new int[]{ AreaNames.Z }" +
+"\n				,new SpikeMerger() {" +
+"\n					public byte[] merge(HashMap<Integer, byte[]> spikes) {" +
+"\n						byte[] elRetorno = null;" + 
+"\n						return elRetorno;" +
+"\n					}" +
+"\n				}));" +
 "\n	}" +
 "\n	" +
 "\n	public void init(){" +
